712cc54cd26dd2119ccfa5d04a039c98c9e762fc
AlteredCow <austint319@gmail.com>
2017-06-28 23:47:50 -0400

    cosmetic changes
    quick brushing up of a number of small quirks

---
+++ docs/_notes/tuckerware_notes.java
@@ -0,0 +1,25 @@
+EASY
+	% namespace / encapsulate
+	* --> bottom of wide_panel dominant pages [ factory for wide variable domain ?]
+	* documentation
+	% switch <classic> tags to more <semantic> ones
+	* css @TODO
+	* :M social icons + final words
+	* email to email function
+	* panel pages as ng-repeats (with script arrays contained locally?)
+	* separate templates and scripts to modulated files
+	* add some animations (eg flying text)
+	* home buttons to rightside
+	! main image scale down
+
+SOME LENGTH
+	* brand self a logo [banner + tab] >> AT merge
+	* modulate more -- CSS, controllers
+	* compress and cleanup
+	* generalize menuGlow for @style/..., by glow(fx)
+	* @scope.loadImages: build ng filters (or sorting machines) per medium, date, ...
+
+MUSINGS
+	{} kap_const. calc :: show all (through animation)
+	{} bayle's theorem
+	{} include project euler solutions
